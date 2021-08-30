7ba7e79e66f45b5a8c0c62aa3aa3eaccb5189803
Akash Dutta <Akash.Dutta2@mindtree.com>
2021-08-30 12:52:52 +0530

    Initial Commit

---
+++ src/test/java/com/mindtree/customerproject/customerproject/CustomerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.customerproject.customerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
